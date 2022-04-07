932cb6c3c4f9c0b856696a797855ad744a2f1cc2
root <root@yakuzaramen.localdomain>
2022-04-07 22:22:42 +0300

    first commit

---
+++ news/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_At = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photos/%y/%m/%d/')
+    is_published = models.BooleanField(default=True)
